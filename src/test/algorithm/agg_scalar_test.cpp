9e2092e1a9a6c2cfd876037be9970465ecfdf34e
The Rocket Sheep <260898324+the-rocket-sheep@users.noreply.github.com>
2026-04-12 14:03:53 +0200

    refact: Fatorize test functions

--- src/test/algorithm/agg_scalar_test.cpp
+++ src/test/algorithm/agg_scalar_test.cpp
@@ -8,38 +8,35 @@
 
 using namespace playground;
 
-TEST(agg_scalar, sum_scalar_naive)
+static void _test_sum_scalar(uint32_t (*function)(const std::vector<uint32_t>&))
 {
     const size_t n = 1023; // forcing a non-power of two
-    std::vector<uint64_t> input(n);
+    std::vector<uint32_t> input(n);
     std::iota(input.begin(), input.end(), 1);
 
-    const uint64_t sum = sum_scalar_naive(input.begin(), input.end());
+    const uint32_t sum = function(input);
 
     // Sum of N first integers is N * (N + 1) / 2
     EXPECT_EQ(sum, n * (n + 1) / 2);
+}
+
+TEST(agg_scalar, sum_scalar_naive)
+{
+    _test_sum_scalar([](const auto& input) {
+        return sum_scalar_naive(input.begin(), input.end());
+    });
 }
 
 TEST(agg_scalar, sum_scalar_unrolled)
 {
-    const size_t n = 1023; // forcing a non-power of two
-    std::vector<uint64_t> input(n);
-    std::iota(input.begin(), input.end(), 1);
-
-    const uint64_t sum = sum_scalar_unrolled<4>(input.begin(), input.end());
-
-    // Sum of N first integers is N * (N + 1) / 2
-    EXPECT_EQ(sum, n * (n + 1) / 2);
+    _test_sum_scalar([](const auto& input) {
+        return sum_scalar_unrolled<4>(input.begin(), input.end());
+    });
 }
 
 TEST(agg_scalar, sum_scalar_auto_vectorized)
 {
-    const size_t n = 1023; // forcing a non-power of two
-    std::vector<uint64_t> input(n);
-    std::iota(input.begin(), input.end(), 1);
-
-    const uint64_t sum = sum_scalar_auto_vectorized(input.begin(), input.end());
-
-    // Sum of N first integers is N * (N + 1) / 2
-    EXPECT_EQ(sum, n * (n + 1) / 2);
+    _test_sum_scalar([](const auto& input) {
+        return sum_scalar_auto_vectorized(input.begin(), input.end());
+    });
 }
