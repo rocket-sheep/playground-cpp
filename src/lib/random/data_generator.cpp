7b871b1a4bfeb6381ddd196fba397a19cc1615d3
The Rocket Sheep <260898324+the-rocket-sheep@users.noreply.github.com>
2026-05-08 11:53:32 +0200

    refact: More generic random data generator

--- src/lib/random/data_generator.cpp
+++ src/lib/random/data_generator.cpp
@@ -45,37 +45,23 @@ std::vector<T> random_data_internal(
 
 } // anonymous namespace
 
-std::vector<uint64_t> DataGenerator::range_ui64(
-    uint64_t lower_bound_inclusive,
-    uint64_t upper_bound_exclusive,
+template <typename T>
+std::vector<T> DataGenerator::range(
+    T lower_bound_inclusive,
+    T upper_bound_exclusive,
     float density)
 {
     return range_internal(_random_engine, lower_bound_inclusive, upper_bound_exclusive, density);
 }
 
-std::vector<uint32_t> DataGenerator::range_ui32(
-    uint32_t lower_bound_inclusive,
-    uint32_t upper_bound_exclusive,
-    float density)
-{
-    return range_internal(_random_engine, lower_bound_inclusive, upper_bound_exclusive, density);
-}
-
-std::vector<uint64_t> DataGenerator::random_data_ui64(
-    size_t size,
-    uint64_t lower_bound_inclusive,
-    uint64_t upper_bound_inclusive)
-{
-    return random_data_internal(_random_engine, size, lower_bound_inclusive, upper_bound_inclusive);
-}
-
-std::vector<uint32_t> DataGenerator::random_data_ui32(
-    size_t size,
-    uint32_t lower_bound_inclusive,
-    uint32_t upper_bound_inclusive)
-{
-    return random_data_internal(_random_engine, size, lower_bound_inclusive, upper_bound_inclusive);
-}
+template std::vector<uint64_t> DataGenerator::range(uint64_t, uint64_t, float);
+template std::vector<uint32_t> DataGenerator::range(uint32_t, uint32_t, float);
+template std::vector<uint16_t> DataGenerator::range(uint16_t, uint16_t, float);
+template std::vector<uint8_t> DataGenerator::range(uint8_t, uint8_t, float);
+template std::vector<int64_t> DataGenerator::range(int64_t, int64_t, float);
+template std::vector<int32_t> DataGenerator::range(int32_t, int32_t, float);
+template std::vector<int16_t> DataGenerator::range(int16_t, int16_t, float);
+template std::vector<int8_t> DataGenerator::range(int8_t, int8_t, float);
 
 template <typename T>
 std::vector<T> DataGenerator::random_data(
@@ -86,7 +72,36 @@ std::vector<T> DataGenerator::random_data(
     return random_data_internal(_random_engine, size, lower_bound_inclusive, upper_bound_exclusive);
 }
 
+template std::vector<uint64_t> DataGenerator::random_data<uint64_t>(size_t, uint64_t, uint64_t);
+template std::vector<uint32_t> DataGenerator::random_data<uint32_t>(size_t, uint32_t, uint32_t);
+template std::vector<uint16_t> DataGenerator::random_data<uint16_t>(size_t, uint16_t, uint16_t);
+template std::vector<uint8_t> DataGenerator::random_data<uint8_t>(size_t, uint8_t, uint8_t);
 template std::vector<int64_t> DataGenerator::random_data<int64_t>(size_t, int64_t, int64_t);
 template std::vector<int32_t> DataGenerator::random_data<int32_t>(size_t, int32_t, int32_t);
 template std::vector<int16_t> DataGenerator::random_data<int16_t>(size_t, int16_t, int16_t);
 template std::vector<int8_t> DataGenerator::random_data<int8_t>(size_t, int8_t, int8_t);
+
+std::vector<std::string> DataGenerator::random_strings(
+    size_t count,
+    size_t min_length,
+    size_t max_length)
+{
+    assert(max_length <= 16);
+
+    std::vector<std::string> result;
+    result.reserve(count);
+
+    std::uniform_int_distribution<size_t> length_distribution(min_length, max_length);
+    char tmp[20];
+    for (size_t i = 0; i < count; ++i) {
+        const size_t length = length_distribution(_random_engine);
+        const uint64_t value = _random_engine();
+
+        // TODO: Use Base64 rather than hexadecimal for more varied output
+        snprintf(tmp, sizeof(tmp), "%016llx", value);
+        tmp[length] = '\0';
+        result.push_back(tmp);
+    }
+
+    return result;
+}
--- src/lib/random/data_generator.hpp
+++ src/lib/random/data_generator.hpp
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 #include <limits>
+#include <string>
+#include <vector>
 
 #include "random/random.hpp"
 
@@ -18,25 +20,11 @@ public:
     inline RandomEngine& random_engine() { return _random_engine; }
 
 public:
-    std::vector<uint64_t> range_ui64(
-        uint64_t lower_bound_inclusive,
-        uint64_t upper_bound_exclusive,
+    template <typename T>
+    std::vector<T> range(
+        T lower_bound_inclusive,
+        T upper_bound_exclusive,
         float density);
-
-    std::vector<uint32_t> range_ui32(
-        uint32_t lower_bound_inclusive,
-        uint32_t upper_bound_exclusive,
-        float density);
-
-    std::vector<uint64_t> random_data_ui64(
-        size_t size,
-        uint64_t lower_bound_inclusive = 0,
-        uint64_t upper_bound_inclusive = std::numeric_limits<uint64_t>::max());
-
-    std::vector<uint32_t> random_data_ui32(
-        size_t size,
-        uint32_t lower_bound_inclusive = 0,
-        uint32_t upper_bound_inclusive = std::numeric_limits<uint32_t>::max());
 
     template <typename T>
     std::vector<T> random_data(
@@ -44,23 +32,10 @@ public:
         T lower_bound_inclusive = 0,
         T upper_bound_inclusive = std::numeric_limits<T>::max());
 
-    template <>
-    inline std::vector<uint64_t> random_data(
-        size_t size,
-        uint64_t lower_bound_inclusive,
-        uint64_t upper_bound_inclusive)
-    {
-        return random_data_ui64(size, lower_bound_inclusive, upper_bound_inclusive);
-    }
-
-    template <>
-    inline std::vector<uint32_t> random_data(
-        size_t size,
-        uint32_t lower_bound_inclusive,
-        uint32_t upper_bound_inclusive)
-    {
-        return random_data_ui32(size, lower_bound_inclusive, upper_bound_inclusive);
-    }
+    std::vector<std::string> random_strings(
+        size_t count,
+        size_t min_length = 4,
+        size_t max_length = 16);
 
 private:
     RandomEngine _random_engine;
